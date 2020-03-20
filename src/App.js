5c62d7ecb6ebe2559bf2ca80b1a13b79812a5288
Agam More <agam.more@demisto.com>
2020-03-20 12:26:35 +0200

    DigiServe

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ export default function Main() {
       <AppBar position="relative">
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap className={classes.appBarTitle}>
-            שירותים IL
+            DigiServe
           </Typography>
           <Button className={classes.appBarButton} onClick={addService}>הוסף שירות</Button>
         </Toolbar>
@@ -105,7 +105,7 @@ export default function Main() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              שירותים IL
+              DigiServe
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               שירותים דיגיטליים בהישג יד
